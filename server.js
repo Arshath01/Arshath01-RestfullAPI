b5fae0cc8e8919231eab05bbaf19b0bfae1e40c8
Arshath Ahamed <ahamedarshath35@gmail.com>
2023-05-14 20:15:54 +0530

    updation

--- server.js
+++ server.js
@@ -9,11 +9,16 @@ app.use(cors())
 
 
 
-app.post('/api/data',(req,res)=>{
+app.post('/api/data',async(req,res)=>{
+    try{
+        const clientResponse = await makeConnection();
     const {name,password} = req.body;
     console.log(name,password)
     
-    res.status(200).json({message:'data recieved succesfully'})
+    res.status(200).json({message:'message from server',name:name,password:password})
+    }catch(err){
+        res.status(404).json({error:"error occured"})
+    }
 })
 
 app.get('/api/data',(req,res)=>{
@@ -21,7 +26,6 @@ app.get('/api/data',(req,res)=>{
     res.status(200).json({message:'success'})
 })
 
-app.listen(port,async()=>{
-    const clientResponse = await makeConnection();
+app.listen(port,()=>{
     console.log(`server is running on the port ${port}`)
 })
