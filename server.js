6ec44963ddbf3a0b9f19bf7e5b68aae37d80409a
Arshath Ahamed <ahamedarshath35@gmail.com>
2023-05-14 20:24:32 +0530

    new

--- server.js
+++ server.js
@@ -10,20 +10,18 @@ app.use(cors())
 
 
 app.post('/api/data',async(req,res)=>{
-    try{
-        const clientResponse = await makeConnection();
+    
+    const clientResponse = await makeConnection();
     const {name,password} = req.body;
     console.log(name,password)
     
     res.status(200).json({message:'message from server',name:name,password:password})
-    }catch(err){
-        res.status(404).json({error:"error occured"})
-    }
+   
 })
 
 app.get('/api/data',(req,res)=>{
   
-    res.status(200).json({message:'success'})
+    res.status(200).json({message:'message'})
 })
 
 app.listen(port,()=>{
