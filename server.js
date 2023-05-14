a2fb7bf4a00814d2867d4f4296c33a78f047251f
Arshath Ahamed <ahamedarshath35@gmail.com>
2023-05-14 21:19:51 +0530

    mongodb

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ app.use(express.json());
 app.use(cors());
 
 // Add the connection to MongoDB before starting the server
-// (async () => {
-//   try {
-//     await makeConnection(); // Modify this function to use environment variables for MongoDB connection details
-//     app.listen(port, () => {
-//       console.log(`Server is running on port ${port}`);
-//     });
-//   } catch (error) {
-//     console.error('Failed to connect to MongoDB:', error);
-//   }
-// })();
+(async () => {
+  try {
+    await makeConnection(); // Modify this function to use environment variables for MongoDB connection details
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+  }
+})();
 
 app.post('/api/data', async (req, res) => {
   const { name, password } = req.body;
